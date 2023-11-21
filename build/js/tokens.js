27788376a2b1ed54dd9471f0dcc3f25fe0de0619
Valdecir Teixeira <vardeba@hotmail.com>
2023-11-21 18:56:09 -0300

    first commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 21 Nov 2023 21:51:32 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorPurple": "#8257e6",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#7a7373",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214"
+};
